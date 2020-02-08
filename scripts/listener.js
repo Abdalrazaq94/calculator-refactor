dac5cbf4d6812a1e5673da6672b930e43cc5d827
abd#94 <abdalrazaqaltaih@gmail.com>
2020-02-08 20:22:56 +0100

    listner

---
+++ scripts/listener.js
@@ -0,0 +1,22 @@
+function calculateHandler(x, y, op) {
+    try {
+      // read & process user input from event
+      x = parseInt(x);
+      y = parseInt(y);
+      op = op.toString();
+      // pass user input through core logic
+  
+      var calculate;
+      calculate = doMath(op, x, y);
+      // render output to DOM for user
+      document.querySelector("#results").innerText =
+        x + " " + op + " " + y + " " + " = " + calculate;
+      // log user action for developers
+      console.log(calculate);
+      // return true for the browser
+      return true;
+    }
+    catch (err) {
+      document.getElementById("results").innerHTML = err.message;
+    }
+  }
